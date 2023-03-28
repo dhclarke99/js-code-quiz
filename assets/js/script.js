4b097b38c38132ca83089879df549a1f8052aafd
dhclarke99 <dhclarke99@gmail.com>
2023-03-27 23:30:44 -0400

    added style display none after start button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,44 @@
-var button = document.querySelectorAll(".answer");
+var startButton = document.querySelector(".start")
 
-var section = document.querySelectorAll("section");
+var introSect = document.querySelector(".intro")
+
+var questions = document.querySelectorAll(".question")
+
+var sections = document.querySelectorAll("section")
 
 
-for (var i = 0; i < button.length; i++) {
 
 
-
-    button[i].addEventListener("click", function (event) {
+    startButton.addEventListener("click", function (event) {
         var element = event.target;
 
-        if (element.matches(".answer")) {
-            console.log("click")
-            var state = section.getAttribute("data-state");
-            console.log(state)
+        if (element.matches(".start")) {
+           
+            var state = introSect.getAttribute("data-state");
+           
 
             if (state === "hidden") {
-                element.textcontent = "";
-            } else {
-                document.querySelector(".intro").textcontent = "";
-                document.querySelector(".intro").setAttribute("display", "none")
-                document.querySelector(".intro").setAttribute("data-state", "hidden");
+                
+                introSect.textcontent = "";
+            } if (state === "visible") {
+               introSect.setAttribute("style", "display: none");
+               
+               introSect.setAttribute("data-state", "hidden");
             }
 
         }
     })
-}
+
+
+//GIVEN I am taking a code quiz
+//WHEN I click the start button
+//THEN a timer starts and I am presented with a question
+//WHEN I answer a question
+//THEN I am presented with another question
+//WHEN I answer a question incorrectly
+//THEN time is subtracted from the clock
+//WHEN all questions are answered or the timer reaches 0
+//THEN the game is over
+//WHEN the game is over
+//THEN I can save my initials and score
+
