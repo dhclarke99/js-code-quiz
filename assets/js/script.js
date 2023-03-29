77329e19853ad3fca845bc1af82f55e0230be826
dhclarke99 <dhclarke99@gmail.com>
2023-03-28 20:15:26 -0400

    added 10 second deduction logic for all questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var submitButton = document.querySelector(".submit")
 var restartButton = document.querySelector(".restart")
 var introSect = document.querySelector(".intro")
 var timeEl = document.querySelector(".timer")
-var secondsLeft = 15
+var secondsLeft = 30
 
 
 //listen for start button and reveal first question plus start timer
@@ -97,13 +97,13 @@ startButton.addEventListener("click", function (event) {
 
 
 
-//listen for answer button click and reveal second question
+//listen for answer button click, hide first question, reveal second question.
 for (i = 0; i < q1Button.length; i++) {
 q1Button[i].addEventListener("click", function (event) {
     var q = document.querySelector(".question1")
     var element = event.target;
     console.log("click test")
-    if (element.matches(".q1") && !element.matches("#correct-answer")) {
+    if (element.matches(".q1") && !element.matches("#correct-answer-1")) {
 
         var state = q.getAttribute("data-state");
         secondsLeft = secondsLeft - 10;
@@ -120,7 +120,7 @@ q1Button[i].addEventListener("click", function (event) {
             document.querySelector(".question2").setAttribute("style", "display: block");
             document.querySelector(".question2").setAttribute("data-state", "visible");
 
-    }} else if (element.matches("#correct-answer")){
+    }} else if (element.matches("#correct-answer-1")){
         var state = q.getAttribute("data-state");
 
 
@@ -142,89 +142,140 @@ q1Button[i].addEventListener("click", function (event) {
 
 })}
 
+//listen for answer button click, hide second question, reveal third question.
 for (i = 0; i < q2Button.length; i++) {
-q2Button[i].addEventListener("click", function (event) {
-    var q = document.querySelector(".question2")
-    var element = event.target;
-    console.log("click test")
-    if (element.matches(".q2")) {
-
-        var state = q.getAttribute("data-state");
-
-
-        if (state === "hidden") {
-
-
-        } if (state === "visible") {
-            q.setAttribute("style", "display: none");
-
-            q.setAttribute("data-state", "hidden");
+    q2Button[i].addEventListener("click", function (event) {
+        var q = document.querySelector(".question2")
+        var element = event.target;
+        console.log("click test")
+        if (element.matches(".q2") && !element.matches("#correct-answer-2")) {
+    
+            var state = q.getAttribute("data-state");
+            secondsLeft = secondsLeft - 10;
+            if (state === "hidden") {
+    
+    
+            } if (state === "visible") {
+                q.setAttribute("style", "display: none");
+    
+                q.setAttribute("data-state", "hidden");
+            }
+            var state = document.querySelector(".question3").getAttribute("data-state")
+            if (state === "hidden") {
+                document.querySelector(".question3").setAttribute("style", "display: block");
+                document.querySelector(".question3").setAttribute("data-state", "visible");
+    
+        }} else if (element.matches("#correct-answer-2")){
+            var state = q.getAttribute("data-state");
+    
+    
+            if (state === "hidden") {
+    
+    
+            } if (state === "visible") {
+                q.setAttribute("style", "display: none");
+    
+                q.setAttribute("data-state", "hidden");
+            }
+    
         }
+        var state = document.querySelector(".question3").getAttribute("data-state")
+        if (state === "hidden") {
+            document.querySelector(".question3").setAttribute("style", "display: block");
+            document.querySelector(".question3").setAttribute("data-state", "visible");
+        }
+    
+    })}
 
-    }
-    var state = document.querySelector(".question3").getAttribute("data-state")
-    if (state === "hidden") {
-        document.querySelector(".question3").setAttribute("style", "display: block");
-        document.querySelector(".question3").setAttribute("data-state", "visible");
-    }
-
-})}
-
+//listen for answer button click, hide third question, reveal fourth question.
 for (i = 0; i < q3Button.length; i++) {
-q3Button[i].addEventListener("click", function (event) {
-    var q = document.querySelector(".question3")
-    var element = event.target;
-    console.log("click test")
-    if (element.matches(".q3")) {
-
-        var state = q.getAttribute("data-state");
-
-
-        if (state === "hidden") {
-
-
-        } if (state === "visible") {
-            q.setAttribute("style", "display: none");
-
-            q.setAttribute("data-state", "hidden");
+    q3Button[i].addEventListener("click", function (event) {
+        var q = document.querySelector(".question3")
+        var element = event.target;
+        console.log("click test")
+        if (element.matches(".q3") && !element.matches("#correct-answer-3")) {
+    
+            var state = q.getAttribute("data-state");
+            secondsLeft = secondsLeft - 10;
+            if (state === "hidden") {
+    
+    
+            } if (state === "visible") {
+                q.setAttribute("style", "display: none");
+    
+                q.setAttribute("data-state", "hidden");
+            }
+            var state = document.querySelector(".question4").getAttribute("data-state")
+            if (state === "hidden") {
+                document.querySelector(".question4").setAttribute("style", "display: block");
+                document.querySelector(".question4").setAttribute("data-state", "visible");
+    
+        }} else if (element.matches("#correct-answer-3")){
+            var state = q.getAttribute("data-state");
+    
+    
+            if (state === "hidden") {
+    
+    
+            } if (state === "visible") {
+                q.setAttribute("style", "display: none");
+    
+                q.setAttribute("data-state", "hidden");
+            }
+    
         }
+        var state = document.querySelector(".question4").getAttribute("data-state")
+        if (state === "hidden") {
+            document.querySelector(".question4").setAttribute("style", "display: block");
+            document.querySelector(".question4").setAttribute("data-state", "visible");
+        }
+    
+    })}
 
-    }
-    var state = document.querySelector(".question4").getAttribute("data-state")
-    if (state === "hidden") {
-        document.querySelector(".question4").setAttribute("style", "display: block");
-        document.querySelector(".question4").setAttribute("data-state", "visible");
-    }
-
-})}
-
+//listen for answer button click, hide fourth question, reveal final score prompt.
 for (i = 0; i < q4Button.length; i++) {
-q4Button[i].addEventListener("click", function (event) {
-    var q = document.querySelector(".question4")
-    var element = event.target;
-    console.log("click test")
-    if (element.matches(".q4")) {
-
-        var state = q.getAttribute("data-state");
-
-
-        if (state === "hidden") {
-
-
-        } if (state === "visible") {
-            q.setAttribute("style", "display: none");
-
-            q.setAttribute("data-state", "hidden");
+    q4Button[i].addEventListener("click", function (event) {
+        var q = document.querySelector(".question4")
+        var element = event.target;
+        console.log("click test")
+        if (element.matches(".q4") && !element.matches("#correct-answer-4")) {
+    
+            var state = q.getAttribute("data-state");
+            secondsLeft = secondsLeft - 10;
+            if (state === "hidden") {
+    
+    
+            } if (state === "visible") {
+                q.setAttribute("style", "display: none");
+    
+                q.setAttribute("data-state", "hidden");
+            }
+            var state = document.querySelector(".question5").getAttribute("data-state")
+            if (state === "hidden") {
+                document.querySelector(".question5").setAttribute("style", "display: block");
+                document.querySelector(".question5").setAttribute("data-state", "visible");
+    
+        }} else if (element.matches("#correct-answer-4")){
+            var state = q.getAttribute("data-state");
+    
+    
+            if (state === "hidden") {
+    
+    
+            } if (state === "visible") {
+                q.setAttribute("style", "display: none");
+    
+                q.setAttribute("data-state", "hidden");
+            }
+    
         }
-
-    }
-    var state = document.querySelector(".question5").getAttribute("data-state")
-    if (state === "hidden") {
-        document.querySelector(".question5").setAttribute("style", "display: block");
-        document.querySelector(".question5").setAttribute("data-state", "visible");
-    }
-
-})}
+        var state = document.querySelector(".question5").getAttribute("data-state")
+        if (state === "hidden") {
+            document.querySelector(".question5").setAttribute("style", "display: block");
+            document.querySelector(".question5").setAttribute("data-state", "visible");
+        }
+    
+    })}
 
 submitButton.addEventListener("click", function (event) {
     var q = document.querySelector(".question5")
